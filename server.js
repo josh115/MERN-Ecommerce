734013ca3d21dc3fae567be1fab1418d2fefa558
Joshua Marsh <joshuamarsh07@outlook.com>
2019-08-19 20:14:48 +0100

    added mongodb connection

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
-app.use(corse());
+app.use(cors());
 app.use(express.json());
 
+const uri = process.env.DATABASE_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Database connection successful")
+})
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
