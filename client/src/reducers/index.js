d192c9a77faa16b1eaca2b494613cf5724761cab
Joshua Marsh <joshuamarsh07@outlook.com>
2019-08-22 15:27:28 +0100

    add new reducers to combinereducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,11 @@
-import { combineReducers } from 'redux';
-import itemReducer from './itemReducer';
-
+import { combineReducers } from "redux";
+import itemReducer from "./itemReducer";
+import errorReducer from "./errorReducer";
+import authReducer from "./authReducer";
 const allReducers = combineReducers({
-    item: itemReducer
+  item: itemReducer,
+  error: errorReducer,
+  auth: authReducer
 });
 
 export default allReducers;
