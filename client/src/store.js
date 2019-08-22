fe49182b54a1d27c8728b93b261b5113c36dca49
Joshua Marsh <joshuamarsh07@outlook.com>
2019-08-22 15:29:24 +0100

    updated to allow redux chrome extention

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import allReducers from './reducers'
+import allReducers from './reducers';
 
 const middleWare = [thunk];
 
-const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
-const store = createStore(allReducers, /* preloadedState, */ composeEnhancers(
-  applyMiddleware(...middleWare)
-));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  allReducers,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(...middleWare))
+);
 
 export default store;
