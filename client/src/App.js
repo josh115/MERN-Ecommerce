28478adf3bc1c4a8efa189826bdb596338790aa9
Joshua Marsh <joshuamarsh07@outlook.com>
2019-08-22 15:28:31 +0100

    add register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ItemsList from './components/ItemsList';
-import Login from './components/Login';
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Register from './components/Register';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/authActions';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <AppNavbar/>
-        <Switch>
-          <Route path="/" exact component={ItemsList}></Route>
-          <Route path="/login" exact component={Login}></Route>
-        </Switch>
-      </Router>
-    </Provider>
-  );
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <AppNavbar />
+          <Switch>
+            <Route path='/' exact component={ItemsList} />
+            <Route path='/register' exact component={Register} />
+          </Switch>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/Register.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { register } from '../actions/authActions';
+
+class Register extends Component {
+  state = {
+    modal: false,
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    msg: null
+  };
+
+  static propTypes = {
+    isAuthenticated: PropTypes.bool,
+    error: PropTypes.object.isRequired,
+    register: PropTypes.func.isRequired
+  };
+
+  componentDidUpdate(prevProps) {
+    const { error } = this.props;
+    if (error !== prevProps.error) {
+      if (error.id === 'REGISTER_FAIL') {
+        this.setState({ msg: error.msg.msg });
+      } else {
+        this.setState({ msg: null });
+      }
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const { firstname, lastname, email, password } = this.state;
+
+    const newUser = {
+      firstname,
+      lastname,
+      email,
+      password
+    };
+    console.log(newUser);
+    this.props.register(newUser);
+  };
+  render() {
+    return (
+      <div className='container login'>
+        {this.state.msg ? (
+          <div className='alert alert-danger' role='alert'>
+            {this.state.msg}
+          </div>
+        ) : null}
+        <form onSubmit={this.onSubmit}>
+          <div className='form-row'>
+            <div className='form-group col-12 col-sm-6'>
+              <label htmlFor='firstname'>First Name</label>
+              <input
+                type='text'
+                name='firstname'
+                className='form-control'
+                placeholder='First name'
+                onChange={this.onChange}
+              />
+            </div>
+            <div className='form-group col-12 col-sm-6'>
+              <label htmlFor='lastname'>Last Name</label>
+              <input
+                type='text'
+                name='lastname'
+                className='form-control'
+                placeholder='Last name'
+                onChange={this.onChange}
+              />
+            </div>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              name='email'
+              className='form-control'
+              placeholder='Email'
+              onChange={this.onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              name='password'
+              className='form-control'
+              placeholder='Password'
+              onChange={this.onChange}
+            />
+          </div>
+          <div className='form-group row justify-content-center mt-4'>
+            <div className='col-12'>
+              <button type='submit' className='btn btn-primary btn-block'>
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error
+});
+
+export default connect(
+  mapStateToProps,
+  { register }
+)(Register);
