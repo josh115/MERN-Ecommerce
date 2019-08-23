2b8126cc9f8b8ff536bd7a629d396fd6265afd8b
Joshua Marsh <joshuamarsh07@outlook.com>
2019-08-23 06:00:59 +0100

    update route to allow image uploads

--- routes/items.js
+++ routes/items.js
@@ -1,16 +1,47 @@
-const router = require("express").Router();
-const Item = require("../models/item.model");
-const auth = require("../middleware/auth");
-router.route("/").get((req, res) => {
-  Item.find()
-    .then(items => res.json(items))
-    .catch(err => res.status(400).json("Error: " + err));
+const router = require('express').Router();
+const Item = require('../models/item.model');
+const auth = require('../middleware/auth');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './client/public/images/stock');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
 });
 
-router.route("/add").post(auth, (req, res) => {
+const imageFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: imageFilter
+});
+
+router.route('/').get((req, res) => {
+  Item.find()
+    .then(items => res.json(items))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post(upload.single('image'), (req, res) => {
   const name = req.body.name;
   const price = Number(req.body.price);
-  const image = req.body.image;
+  const image = req.file.originalname;
   const category = req.body.category;
   const subcategory = req.body.subcategory;
 
@@ -24,8 +55,8 @@ router.route("/add").post(auth, (req, res) => {
 
   newItem
     .save()
-    .then(() => res.json("Item Added!"))
-    .catch(err => res.status(400).json("Error: " + err));
+    .then(() => res.json('Item Added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
