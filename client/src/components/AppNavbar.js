8a366e581fff075e5a17ef5909698ebf9695b2a1
Joshua Marsh <joshuamarsh07@outlook.com>
2019-08-23 05:59:48 +0100

    add authentication to navbar with custom routes

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logout } from '../actions/authActions';
@@ -22,12 +22,54 @@ class AppNavbar extends Component {
   };
 
   render() {
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authDropdownMenu = (
+      <Fragment>
+        <LinkContainer to='/login' exact>
+          <Dropdown.Item>
+            <span>Login</span>
+          </Dropdown.Item>
+        </LinkContainer>
+        <LinkContainer to='/register' exact>
+          <Dropdown.Item>
+            <span>Register</span>
+          </Dropdown.Item>
+        </LinkContainer>
+      </Fragment>
+    );
+
+    const authNavItems = (
+      <Fragment>
+        <LinkContainer to='/stock' exact>
+          <Nav.Link>Edit Stock</Nav.Link>
+        </LinkContainer>
+      </Fragment>
+    );
+    const guestDropdownMenu = (
+      <Fragment>
+        <LinkContainer to='/account' exact>
+          <Dropdown.Item>
+            <span>Account</span>
+          </Dropdown.Item>
+        </LinkContainer>
+        <LinkContainer to='/orders' exact>
+          <Dropdown.Item>
+            <span>Order History</span>
+          </Dropdown.Item>
+        </LinkContainer>
+        <Dropdown.Item>
+          <span onClick={this.props.logout}>Logout</span>
+        </Dropdown.Item>
+      </Fragment>
+    );
+
     return (
       <Navbar bg='dark' variant='dark'>
         <LinkContainer to='/' exact>
           <Navbar.Brand>Shop Name</Navbar.Brand>
         </LinkContainer>
-        <Nav className='mr-auto' />
+        <Nav className='mr-auto'>{isAuthenticated ? authNavItems : null}</Nav>
         <Nav>
           <Nav.Link href='/basket'>
             <img src='./images/basket.svg' alt='' />
@@ -36,22 +78,21 @@ class AppNavbar extends Component {
             <img src='./images/search.svg' alt='' />
           </Nav.Link>
           <Nav.Item>
+            {isAuthenticated ? (
+              <span className='navbar-text ml-1' id='welcome-msg'>
+                <strong>{user.firstname}</strong>
+              </span>
+            ) : (
+              ''
+            )}
+          </Nav.Item>
+          <Nav.Item>
             <Dropdown alignRight>
-              <Dropdown.Toggle id='nav-dropdown'>
+              <Dropdown.Toggle id='account-dropdown'>
                 <img src='./images/user.svg' alt='' />
               </Dropdown.Toggle>
-
               <Dropdown.Menu>
-                <LinkContainer to='/login' exact>
-                  <Dropdown.Item>
-                    <span>Login</span>
-                  </Dropdown.Item>
-                </LinkContainer>
-                <LinkContainer to='/register' exact>
-                  <Dropdown.Item>
-                    <span>Register</span>
-                  </Dropdown.Item>
-                </LinkContainer>
+                {isAuthenticated ? guestDropdownMenu : authDropdownMenu}
               </Dropdown.Menu>
             </Dropdown>
           </Nav.Item>
