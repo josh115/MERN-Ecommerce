4e1c4e797e7a2865bd9c2d8f6a3aa300df9223da
Joshua Marsh <joshuamarsh07@outlook.com>
2019-08-24 05:57:13 +0100

    changed item box css to be more responsive

--- client/src/components/ItemsList.js
+++ client/src/components/ItemsList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getItems } from '../actions/itemActions';
 import PropTypes from 'prop-types';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Form, Col, Row } from 'react-bootstrap';
 import SideBar from './SideBar';
 class ItemsList extends Component {
   constructor(props) {
@@ -18,68 +18,71 @@ class ItemsList extends Component {
     this.props.getItems();
   }
 
-  onQuantityChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
+  onQuantityChange(val) {
+    return val;
+  }
 
   render() {
     const { items } = this.props.item;
     return (
       <div className='d-flex flex-row justify-content-center'>
         <div className='item-list'>
-          <div className='row'>
+          <Row>
             {items.map(({ _id, name, image, price }) => (
               <div
                 className='col-lg-3 col-md-4 col-sm-6 item-container'
                 key={_id}
               >
-                <div className='row flex-row flex-sm-column'>
-                  <div className='item-inner'>
-                    <div className='d-flex justify-content-center mt-2'>
-                      <img
-                        className='item-img'
-                        src={
-                          image
-                            ? `/images/stock/${image}`
-                            : '/images/stock/missing.png'
-                        }
-                        alt=''
-                      />
-                    </div>
-                    <div className='d-flex justify-content-center mt-3'>
+                <div className='item-inner d-flex d-sm-block'>
+                  <div className='d-flex justify-content-center mt-2'>
+                    <img
+                      className='item-img'
+                      src={
+                        image
+                          ? `/images/stock/${image}`
+                          : '/images/stock/missing.png'
+                      }
+                      alt=''
+                    />
+                  </div>
+                  <div className='w-100'>
+                    <div className='d-flex justify-content-sm-center mt-3'>
                       <span>
                         <strong>{name}</strong>
                       </span>
                     </div>
-                    <div className='d-flex justify-content-center mt-3'>
+                    <div className='d-flex justify-content-sm-center mt-3 mb-3'>
                       <span>&pound;{price['$numberDecimal']}</span>
                     </div>
-                    <div className='input-group mb-3 justify-content-around mt-3'>
-                      <form>
-                        <div className='form-group'>
-                          <input
+                    <Form className='mr-3 mx-sm-3'>
+                      <Form.Control name='id' hidden='password' value={_id} />
+                      <Row>
+                        <Col>
+                          <Form.Control
+                            className='pr-1 cartitembtn mb-3'
                             type='number'
                             name='quantity'
-                            className='add-item-btn'
-                            onChange={this.onQuantityChange}
+                            onChange={() => {
+                              this.onQuantityChange(this.state.quantity);
+                            }}
+                            defaultValue={this.state.quantity}
                             autoComplete='off'
-                            value={this.state.quantity}
                             min='1'
                             max='99'
                           />
-                          <button type='button' className='btn btn-primary'>
+                        </Col>
+                        <Col>
+                          <Button variant='primary' className='mb-3' block>
                             Add
-                          </button>
-                        </div>
-                      </form>
-                    </div>
+                          </Button>
+                        </Col>
+                      </Row>
+                    </Form>
                   </div>
                 </div>
               </div>
             ))}
-          </div>
+          </Row>
         </div>
         <SideBar />
       </div>
