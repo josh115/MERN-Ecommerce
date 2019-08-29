c9608217aded5d0a813a97676888ed4a2a011bcf
Joshua Marsh <joshuamarsh07@outlook.com>
2019-08-29 18:55:01 +0100

    add new cart reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,13 @@
-import { combineReducers } from "redux";
-import itemReducer from "./itemReducer";
-import errorReducer from "./errorReducer";
-import authReducer from "./authReducer";
+import { combineReducers } from 'redux';
+import itemReducer from './itemReducer';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer';
+import cartReducer from './cartReducer';
 const allReducers = combineReducers({
   item: itemReducer,
   error: errorReducer,
-  auth: authReducer
+  auth: authReducer,
+  cart: cartReducer
 });
 
 export default allReducers;
