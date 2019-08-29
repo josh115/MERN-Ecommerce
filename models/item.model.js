a246acff0ffea5ff167be81e4e8fc911c2b9f2f2
Joshua Marsh <joshuamarsh07@outlook.com>
2019-08-29 18:50:45 +0100

    change price to Number

--- models/item.model.js
+++ models/item.model.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    price: {
-        type: mongoose.Decimal128,
-        required: true
-    },
-    image: { type: String },
-    category: { type: String },
-    SubCategory: { type: String }
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  image: { type: String },
+  category: { type: String },
+  subcategory: { type: String }
 });
 
 module.exports = Item = mongoose.model('item', ItemSchema);
