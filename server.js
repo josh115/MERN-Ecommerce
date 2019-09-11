eef2b37419ca5f8b76ebae8893c704be48992e2c
Joshua Marsh <joshuamarsh07@outlook.com>
2019-09-11 01:17:00 +0100

    add heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -25,6 +26,14 @@ app.use('/api/items', ItemsRouter);
 
 const UsersRouter = require('./routes/users');
 app.use('/api/users', UsersRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
