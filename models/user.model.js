0c4943108cdd53c3a9b806a957f50766ed65eb5e
Joshua Marsh <joshuamarsh07@outlook.com>
2019-09-28 18:39:18 +0100

    add admin to user table

--- models/user.model.js
+++ models/user.model.js
@@ -1,25 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  firstname: {
-    type: String,
-    required: true
-  },
-  lastname: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+  email: { type: String, required: true, unique: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  password: { type: String, required: true },
+  admin: { type: Boolean, default: false }
 });
 
-module.exports = User = mongoose.model("user", UserSchema);
+module.exports = User = mongoose.model('user', UserSchema);
